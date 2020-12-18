c098d6f5bf610a478dc90697de64229aa83af6f1
Matt Byrd <mbyrd7886@outlook.com>
2020-12-17 23:14:10 -0500

    added styling to additional dropdown, added additional custom event for officer select, modified CriminalList.js to filter by arresting officer

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,28 +1,43 @@
 import { getCriminals, useCriminals } from './CriminalProvider.js'
 import { Convict } from './Criminal.js'
 import { useConvictions } from '../convictions/ConvictionProvider.js'
+import { useOfficers } from '../officers/OfficerProvider.js'
 
 const eventHub = document.querySelector('.container');
 const contentTarget = document.querySelector('.criminalsContainer');
 
 
 eventHub.addEventListener('crimeChosen', event => {
-        const chosenCrimeId = event.detail.crimeThatWasChosen;
-        const convictions = useConvictions();
-        const crimeObj = convictions.find(crime => {
-            return parseInt(chosenCrimeId) === crime.id
-        });
-
-        const allCriminals = useCriminals();
-        const matchingCriminals = allCriminals.filter(criminal => {
-            return crimeObj.name === criminal.conviction
-        })
-        console.log(matchingCriminals)
-
-        render(matchingCriminals);
+    const chosenCrimeId = event.detail.crimeThatWasChosen;
+    const convictions = useConvictions();
+    const crimeObj = convictions.find(crime => {
+        return parseInt(chosenCrimeId) === crime.id
+    });
+    const allCriminals = useCriminals();
+    const matchingCriminals = allCriminals.filter(criminal => {
+        return crimeObj.name === criminal.conviction
+    })
+    console.log(matchingCriminals)
+    render(matchingCriminals);
 
     }
-)
+);
+
+eventHub.addEventListener('officerSelected', event => {
+    const chosenOfficerId = parseInt(event.detail.officer); //officerID
+    const policeOfficers = useOfficers();
+    const officerName = policeOfficers.find(cop => {
+        return chosenOfficerId === cop.id
+    });
+    
+    const allCriminals = useCriminals();
+    const matchingCriminals = allCriminals.filter(criminal => {
+        return officerName.name === criminal.arrestingOfficer
+    });
+    console.log(matchingCriminals);
+
+    render(matchingCriminals);
+})
 
 const render = criminalCollection => {
     contentTarget.innerHTML = ''
---
+++ scripts/officers/OfficerProvider.js
@@ -0,0 +1,9 @@
+let officers = [];
+
+export const useOfficers = () => officers.slice();
+
+export const getOfficers = () => {
+    return fetch("https://criminals.glassdale.us/officers")
+        .then(response => response.json())
+        .then(parsedOfficers => officers = parsedOfficers)
+}
