53644a68ca4b3547b913852e482b605b45794001
Matt Byrd <mbyrd7886@outlook.com>
2021-01-07 23:40:24 -0500

    Added criminal list dropdown for notes and name for title

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -1,6 +1,6 @@
-export const Note = (noteData) => {
+export const Note = (noteData, criminalName) => {
     return `<section class="note">
-        <h4 class="note--title">${noteData.title}</h4>
+        <h4 class="note--title">${criminalName.name}</h4>
         <div class="note--author"><strong>Author: </strong>${noteData.author}</div>
         <div class="note--content">${noteData.content}</div>
         <div class="note--date"><strong>Date: </strong>${new Date(noteData.timestamp).toLocaleDateString('en-US')}</div>
--- scripts/notes/NoteList.js
+++ scripts/notes/NoteList.js
@@ -1,19 +1,29 @@
 import { Note } from './Note.js'
 import { getNotes, useNotes } from './NoteProvider.js'
+import { getCriminals, useCriminals } from '../criminals/CriminalProvider.js'
 
 const contentTarget = document.querySelector('.noteListContainer')
 const eventHub = document.querySelector('.container')
 
-const render = notes => {
-    contentTarget.innerHTML = notes.map(note => Note(note)).join('');
+//const render = (notes) => {
+//    contentTarget.innerHTML = notes.map(note => Note(note)).join('');
+//}
+const render = (noteList, criminalList) => {
+    contentTarget.innerHTML = noteList.map(note => {
+        const matchingCriminal = criminalList.find(criminal => criminal.id === note.criminalId)
+        return Note(note, matchingCriminal);
+    })
 }
+
 
 const NoteList = () => {
     getNotes()
+        .then(getCriminals)
         .then(() => {
-            const allNotes = useNotes();
-            render(allNotes);
-        });
+            const notes = useNotes();
+            const criminals = useCriminals();
+            render(notes, criminals);
+        })
 }
 
 eventHub.addEventListener('showNotesClicked', NoteList);
